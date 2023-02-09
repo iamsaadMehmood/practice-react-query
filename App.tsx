afb1876b414ab5c52d0aea4313c7f7c79424ed29
Saad Mehmood <saad.mehmood@aquila360.com>
2023-02-09 12:50:43 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
